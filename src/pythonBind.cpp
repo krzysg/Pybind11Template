222a9ea8acfad4087bccdfa14c55038a169ed423
Krzysztof Gonciarz <gonciarz@mpi-cbg.de>
2018-06-27 15:21:37 +0200

    Added test impl. with ref increasing

--- src/pythonBind.cpp
+++ src/pythonBind.cpp
@@ -1,4 +1,5 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
 #include <iostream>
 #include <sstream>
 
@@ -14,20 +15,36 @@ template <typename T>
 class MyClass {
     std::unique_ptr<T[]> iData;
     size_t iSize;
+    bool releaseMemory;
 
 public:
 
     MyClass (size_t size) : iSize(size) {
         iData.reset( new T[size] );
+        releaseMemory = true;
         for (size_t i = 0; i < size; ++i) iData[i] = 2 * (i + 1);
     }
-    ~MyClass() { std::cout << "~MyClass[" << iSize << "]" << std::endl; }
+    ~MyClass() {
+        std::cout << "~MyClass[" << iSize << "]" << std::endl;
+        if (!releaseMemory) iData.release();
+    }
 
     size_t size() const { return iSize; }
     T *data() { return iData.get(); }
 
     T& operator[](size_t idx) { return iData[idx]; }
     const T& operator[](size_t idx) const { return iData[idx]; }
+
+    void setNew(T *newData, size_t newSize) {
+        if (releaseMemory) {
+            std::cout << "released MyClass[" << iSize << "]" << std::endl;
+            iData.reset();
+        }
+        else iData.release();
+        iData.reset(newData);
+        iSize = newSize;
+        releaseMemory = false;
+    }
 
     std::string toString() {
         std::ostringstream s;
@@ -49,6 +66,13 @@ void AddMyClass(pybind11::module &m, const std::string &aTypeString) {
     py::class_<MyType>(m, typeStr.c_str(), py::buffer_protocol())
             .def(py::init<size_t>())
             .def("__repr__", &MyType::toString, "Default repr function")
+            .def("setNew", [](MyType &s, py::array_t<DataType, py::array::c_style> &a) {
+                s.setNew(a.mutable_data(0), a.size());
+                std::cout << "PYTHON OWNS DATA: " << a.owndata() << std::endl;
+                std::cout << "PYTHON REFS: " << a.ref_count() << std::endl;
+                a.inc_ref();
+                std::cout << "PYTHON REFS: " << a.ref_count() << std::endl;
+            })
             .def("__getitem__", [](const MyType &s, size_t i) {
                 if (i >= s.size()) throw py::index_error();
                 return s[i];
