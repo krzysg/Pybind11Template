bad0e9b0be1f23213c00f6fad699cb4573db3a22
Krzysztof Gonciarz <gonciarz@mpi-cbg.de>
2018-06-27 12:46:13 +0200

    Test example updated

--- src/test.py
+++ src/test.py
@@ -11,16 +11,26 @@ print("\n\n--------------- SHARED MEM --------------------")
 img=np.array(x, copy = False)
 print(img)
 print(x)
+
+print("Changing first element of np array to 123")
 img[0] = 123;
 print(img)
 print(x)
+
+print("Changing first element of cpp array to 666")
+x[0] = 666;
+print(img)
+print(x)
+
 
 print("\n\n------------------- COPY ----------------------")
 # Copying object
 img=np.array(x, copy = True)
 print(img)
 print(x)
-img[0] = 666;
+print("Changing first element of np array to 10000")
+img[0] = 10000;
 print(img)
 print(x)
 
+print("\n\n-----------------  DONE -----------------------")
