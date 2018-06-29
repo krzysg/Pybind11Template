935865cc22a395655a206c8bf4f3e1e3638e48c7
Krzysztof Gonciarz <gonciarz@mpi-cbg.de>
2018-06-29 13:39:12 +0200

    Working copyless numpy -> c++

--- src/pythonBind.cpp
+++ src/pythonBind.cpp
@@ -12,47 +12,61 @@ namespace py = pybind11;
 
 // -------- Class to be wrapped in python ----------------------------
 template <typename T>
-class MyClass {
+class MyVectorClass {
     std::unique_ptr<T[]> iData;
     size_t iSize;
-    bool releaseMemory;
+
+    bool releaseMemory = false;
+    pybind11::handle handle;
 
 public:
 
-    MyClass (size_t size) : iSize(size) {
+    MyVectorClass() { iSize = 0; }
+
+    MyVectorClass(size_t size) : iSize(size) {
         iData.reset( new T[size] );
         releaseMemory = true;
-        for (size_t i = 0; i < size; ++i) iData[i] = 2 * (i + 1);
+
+        // init with a 1..size sequence
+        for (size_t i = 0; i < size; ++i) iData[i] = i + 1;
     }
-    ~MyClass() {
-        std::cout << "~MyClass[" << iSize << "]" << std::endl;
-        if (!releaseMemory) iData.release();
+
+    ~MyVectorClass() {
+        releasePython();
     }
 
     size_t size() const { return iSize; }
-    T *data() { return iData.get(); }
 
+    T *data() { return iData.get(); }
+    const T *data() const { return iData.get(); }
     T& operator[](size_t idx) { return iData[idx]; }
     const T& operator[](size_t idx) const { return iData[idx]; }
 
-    void setNew(T *newData, size_t newSize) {
-        if (releaseMemory) {
-            std::cout << "released MyClass[" << iSize << "]" << std::endl;
-            iData.reset();
+    void releasePython() {
+        if (!releaseMemory) {
+            if (handle.ptr() != nullptr) {
+                handle.dec_ref(); // decrease reference counter - it will allow python to free object when not in use
+                handle.ptr() = nullptr;
+            }
+            iData.release(); // release pointer from unique_ptr without freeing
         }
-        else iData.release();
-        iData.reset(newData);
-        iSize = newSize;
+    }
+
+    void setFromPython(T *ptr, size_t size, const pybind11::handle &h) {
+        releasePython();
+        handle.ptr() = h.ptr();
+        iData.reset(ptr);
+        iSize = size;
         releaseMemory = false;
     }
 
     std::string toString() {
         std::ostringstream s;
         s << "CPP: [";
-        for (size_t i = 0; i < size() - 1; ++i) {
-            s << iData[i] << ", ";
+        if (iSize > 0) {
+            for (size_t i = 0; i < iSize - 1; ++i) { s << iData[i] << ", "; }
+            s << iData[iSize - 1];
         }
-        s << iData[size() - 1];
         s << "]";
         return s.str();
     }
@@ -61,37 +75,58 @@ public:
 // -------- Templated wrapper -------------------------------------------------
 template <typename DataType>
 void AddMyClass(pybind11::module &m, const std::string &aTypeString) {
-    using MyType = MyClass<DataType>;
-    std::string typeStr = "MyClass" + aTypeString;
-    py::class_<MyType>(m, typeStr.c_str(), py::buffer_protocol())
-            .def(py::init<size_t>())
-            .def("__repr__", &MyType::toString, "Default repr function")
-            .def("setNew", [](MyType &s, py::array_t<DataType, py::array::c_style> &a) {
-                s.setNew(a.mutable_data(0), a.size());
-                std::cout << "PYTHON OWNS DATA: " << a.owndata() << std::endl;
-                std::cout << "PYTHON REFS: " << a.ref_count() << std::endl;
-                a.inc_ref();
-                std::cout << "PYTHON REFS: " << a.ref_count() << std::endl;
-            })
-            .def("__getitem__", [](const MyType &s, size_t i) {
-                if (i >= s.size()) throw py::index_error();
-                return s[i];
-            })
-            .def("__setitem__", [](MyType &s, size_t i, DataType v) {
-                if (i >= s.size()) throw py::index_error();
-                s[i] = v;
-            })
-            .def("__len__", &MyType::size)
-            .def_buffer([](MyType &a) -> py::buffer_info {
-                return py::buffer_info(
-                        a.data(),
-                        sizeof(DataType),
-                        py::format_descriptor<DataType>::format(),
-                        1,
-                        {a.size()},
-                        {sizeof(DataType)}
-                );
-            });
+    using MyType = MyVectorClass<DataType>;
+    std::string typeStr = "MyVectorClass" + aTypeString;
+    py::class_<MyType> classObj(m, typeStr.c_str(), py::buffer_protocol(), py::dynamic_attr());
+    classObj
+        .def(py::init([=](py::buffer b) {
+            py::buffer_info info = b.request();
+
+            if (info.ndim != 1) throw std::runtime_error("Incompatible buffer dimension!");
+            if (info.format != py::format_descriptor<DataType>::format()) {
+                std::string errMsg = "Incompatible format: expected a <";
+                errMsg += aTypeString;
+                errMsg += "> type vector!";
+                throw std::runtime_error(errMsg);
+            }
+            MyType *m = new MyType();
+            m->setFromPython((DataType*)info.ptr, info.size, b.release());
+            return m;
+        }))
+        .def(py::init<size_t>())
+        .def("__repr__", &MyType::toString)
+        .def("__getitem__", [](const MyType &s, size_t i) {
+            if (i >= s.size()) throw py::index_error();
+            return s[i];
+        })
+        .def("__setitem__", [](MyType &s, size_t i, DataType v) {
+            if (i >= s.size()) throw py::index_error();
+            s[i] = v;
+        })
+        .def("__len__", &MyType::size)
+        .def_buffer([](MyType &a) -> py::buffer_info {
+            return py::buffer_info(
+                    a.data(),
+                    sizeof(DataType),
+                    py::format_descriptor<DataType>::format(),
+                    1,
+                    {a.size()},
+                    {sizeof(DataType)}
+            );
+        })
+        .def("set", [=](MyType &m, py::buffer b) {
+            py::buffer_info info = b.request();
+
+            if (info.ndim != 1) throw std::runtime_error("Incompatible buffer dimension!");
+            if (info.format != py::format_descriptor<DataType>::format()) {
+                std::string errMsg = "Incompatible format: expected a <";
+                errMsg += aTypeString;
+                errMsg += "> type vector!";
+                throw std::runtime_error(errMsg);
+            }
+
+            m.setFromPython((DataType*)info.ptr, info.size, b.release());
+        });
 }
 
 
--- src/test.py
+++ src/test.py
@@ -1,12 +1,14 @@
 from __future__ import print_function
 import numpy as np
+
 from pyTemplate import *
 
-print("----------------- OBJ created -----------------")
-x=MyClassShort(8)
+print("============== SHARING CPP WITH NUMPY =========")
+print("CPP created")
+x=MyVectorClassShort(8)
 print("Size: ", len(x))
 
-print("\n\n--------------- SHARED MEM --------------------")
+print("\n--------------- SHARED MEM --------------------")
 # Sharing memory 
 img=np.array(x, copy = False)
 print(img)
@@ -23,7 +25,7 @@ print(img)
 print(x)
 
 
-print("\n\n------------------- COPY ----------------------")
+print("\n------------------- COPY ----------------------")
 # Copying object
 img=np.array(x, copy = True)
 print(img)
@@ -32,5 +34,33 @@ print("Changing first element of np array to 10000")
 img[0] = 10000;
 print(img)
 print(x)
+
+
+print("\n\n\n============== SHARING NUMPY WITH CPP =========")
+print("----------------- numpy OBJ created -------------")
+n=np.arange(15, dtype=np.uint16)
+print("Size: ", len(n))
+
+print("\n--------------- SHARED MEM --------------------")
+# Sharing memory
+y=MyVectorClassShort(n)
+print(n)
+print(y)
+
+print("Changing first element of np array to 123")
+n[0] = 123;
+print(n)
+print(y)
+
+print("Changing first element of cpp array to 666")
+y[0] = 666;
+print(n)
+print(y)
+
+print("Removing numpy")
+del n
+print(y)
+y[10]=12345
+print(y)
 
 print("\n\n-----------------  DONE -----------------------")
